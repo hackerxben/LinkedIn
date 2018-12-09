803fde65e7655b5f8919fe08640421a376618bdc
hackerxben <hackerxben@gmail.com>
2018-12-09 18:24:57 +0100

    Update linkedin_invitation_sender.py

--- LinkedIn invitation Sender/master/linkedin_invitation_sender.py
+++ LinkedIn invitation Sender/master/linkedin_invitation_sender.py
@@ -1,170 +1,219 @@
 # -*- coding: utf-8 -*-
-import argparse, os,sys, time
-import urlparse, random
+import argparse
+import sys
+import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
+SEARCH_URL = 'https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22cn%3A0%22%2C%22za%3A0%22%2C%22us%3A0%22%2C%22in%3A0%22%2C%22gb%3A0%22%2C%22ch%3A0%22%2C%22au%3A0%22%2C%22fr%3A0%22%2C%22at%3A0%22%2C%22de%3A0%22%2C%22es%3A0%22%2C%22jp%3A0%22%2C%22kr%3A0%22%2C%22ru%3A0%22%5D&facetNetwork=%5B%22S%22%2C%22O%22%5D&origin=FACETED_SEARCH&keywords=&page='
+
+#parse_the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("email", help="linkedin email")
+parser.add_argument("password", help="linkedin password")
+parser.add_argument("searchTerm", help="The field of interest")
+args = parser.parse_args()
+
+#load_the_note_from_file
+note_linkedin = ""
+f = open('note_linkedin.txt','r')
+note_linkedin = f.read()
+f.close()
+note_linkedin = unicode(note_linkedin, errors='replace')
 
 
-def getPeopleLinks(page):
-	links = []
-	for link in page.find_all('a'):
-		url = link.get('href')
-		if url:
-			if 'profile/view?id=' in url:
-				links.append(url)
-	return links
-
-def getJobLinks(page):
-	links = []
-	for link in page.find_all('a'):
-		url = link.get('href')
-		if url:
-			if '/jobs' in url:
-				links.append(url)
-	return links
-
-def getID(url):
-	pUrl = urlparse.urlparse(url)
-	return urlparse.parse_qs(pUrl.query)['id'][0]
-
-
-def ViewBot(browser):
-	visited = {}
-	pList = []
-	count = 0
+def scroll_down(browser):
+	print "++ scrolling down"
+	SCROLL_PAUSE_TIME = 0.5
+	#Get scroll height
+	last_height = browser.execute_script("return document.body.scrollHeight")
 	while True:
-		#sleep to make sure everything loads, add random to make us look human.
-		time.sleep(random.uniform(3.5,6.9))
-		page = BeautifulSoup(browser.page_source)
-		people = getPeopleLinks(page)
-		if people:
-			for person in people:
-				ID = getID(person)
-				if ID not in visited:
-					pList.append(person)
-					visited[ID] = 1
-		if pList: #if there is people to look at look at them
-			person = pList.pop()
-			browser.get(person)
-			count += 1
-		else: #otherwise find people via the job pages
-			jobs = getJobLinks(page)
-			if jobs:
-				job = random.choice(jobs)
-				root = 'http://www.linkedin.com'
-				roots = 'https://www.linkedin.com'
-				if root not in job or roots not in job:
-					job = 'https://www.linkedin.com'+job
-				browser.get(job)
-			else:
-				print "I'm Lost Exiting"
-				break
+		#Scroll down to bottom
+		browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		#Wait to load page
+		time.sleep(SCROLL_PAUSE_TIME)
+		#Calculate new scroll height and compare last scroll height
+		new_height = browser.execute_script("return document.body.scrollHeight")
+		if new_height == last_height:
+			break
+		last_height = new_height
+	print "++ scrolled down"
 
-		#Output (Make option for this)
-		print "[+] "+browser.title+" Visited! \n("\
-			+str(count)+"/"+str(len(pList))+") Visited/Queue)"
+def wait_until_page_loads(browser,count):
+	if count > 9:
+		return "timeout"
+	try:
+		time.sleep(2)
+		browser.find_element_by_css_selector("button.search-result__actions--primary.button-secondary-medium.m5")
+		print "++ page should be loaded now"
+		return
+	except NoSuchElementException as e:
+		try:
+			browser.find_element_by_css_selector("span.message-anywhere-button.button-secondary-medium.search-result__actions--primary.m5.link-without-visited-state")
+			print "++ page should be loaded now"
+			return
+		except NoSuchElementException as e:
+			print "-- page still loading"
+			time.sleep(0.5)
+			wait_until_page_loads(browser,count+1)
 
-def search(browser,searchTerm):
-	browser.get('https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22tn%3A0%22%5D&keywords='+searchTerm+'&origin=FACETED_SEARCH')
-	print "[+] Search Done"
+def wait_until_overlay_appears(browser,count = 0):
+	try:
+		browser.find_element_by_css_selector("div.modal-wormhole.visible.send-invite.ember-view")
+		print "++ found overlay,good to go"
+	except Exception as e:
+		if count > 3:
+			return "reclick"
+		print "-- waiting for overlay"
+		wait_until_overlay_appears(browser,count +1)
 
-def get_list_people(browser):
-	tunisians = []
-	time.sleep(5)
-	page = BeautifulSoup(browser.page_source,"html.parser")
-	people = page.find_all("li",{"class","search-result search-entity search-result--person ember-view"})
-	for person in people:
-		if "Tunis" in str(person) or "Tunisia" in str(person) or "Tunisie" in str(person):
-			#print person.get('id')
-			tunisians.append(person.get('id'))
-	return tunisians
+def click_connect(browser,button,count=0):
+	if count == 3:
+		return "continue"
+	button.click()
+	time.sleep(0.5)
+	result = wait_until_overlay_appears(browser,0)
+	if result == "reclick":
+		print "-- reclicking connect"
+		click_connect(browser,button,count+1)
 
-def send_invitations(browser,msg_linkedin):
-	all_poeple = list(browser.find_elements_by_css_selector('.search-result.search-entity.search-result--person.ember-view'))
-	tunisians = get_list_people(browser)
-	print len(tunisians)
-	for person in all_poeple:
-		for id in tunisians:
-			person = browser.find_element_by_id(id)
-			buttons = list(person.find_elements_by_css_selector('.search-result__actions--primary.button-secondary-medium.m5'))
-			for button in buttons:
-				if "Se connecter" in str(button.text):
-					button.click()
-					secondButton = person.find_element_by_css_selector('.button-secondary-large')
-					if "Ajouter une note" in str(secondButton.text):
-						secondButton.click()
-						textarea = browser.find_element_by_id('custom-message').send_keys(msg_linkedin)
-						sendButton = browser.find_element_by_css_selector('.button-primary-large.ml3')
-						sendButton.click()
-						time.sleep(2)
-						break
-	print "[+] Success! Invitation sent"
+def click_add_note(browser,button,count=0):
+	if count == 3:
+		return "continue"
+	button.click()
+	time.sleep(0.5)
+	try:
+		browser.find_elements_by_id("custom-message")
+	except Exception as e:
+		print "-- reclicking add note"
+		click_add_note(browser,button,count+1)
 
-def get_current_page(browser):
-	pages = browser.find_element_by_css_selector('.page-list')
-	current_page = pages.find_element_by_css_selector('.active')
-	return int(str(current_page.text))
-
-def next_page(browser):
-	browser.find_element_by_css_selector('.next').click()
-	time.sleep(5)
-
-def login(browser,args):
-	emailElement = browser.find_element_by_id("session_key-login")
+def login(browser):
+	print "[+] opening login page"
+	browser.get("https://linkedin.com/uas/login")
+	print "[+] starting login"
+	emailElement = browser.find_element_by_name("session_key")
 	emailElement.send_keys(args.email)
-	passElement = browser.find_element_by_id("session_password-login")
+	passElement = browser.find_element_by_name("session_password")
 	passElement.send_keys(args.password)
 	passElement.submit()
-	time.sleep(5)
+	while browser.title != "LinkedIn":
+		time.sleep(0.5)
+	print "[+] login was successful"
 
-def Main():
+def get_current_page_number(browser):
+	try:
+		page = browser.find_element_by_css_selector("li.page-list").find_element_by_css_selector("ol").find_element_by_css_selector("li.active").text
+	except Exception as e:
+		print "-- probably the page is still loading"
+		time.sleep(0.5)
+		get_current_page_number(browser)
+	return int(page)
+
+def close_overlay(browser):
+	webdriver.ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+	time.sleep(0.5)
+	print "closed overlay"
+
+def clicking(browser,button):
+	if click_connect(browser,button,0) == "continue":
+	  print "skipping..."
+	  return
+	time.sleep(1)
+	print "++ clicked connect"
+	try:
+		browser.find_element_by_id("email")
+		print "-- oups this guy requires an email"
+		close_overlay(browser)
+		return "remove"
+	except Exception as e:
+		pass
+	add_note = browser.find_element_by_css_selector("button.button-secondary-large")
+	if click_add_note(browser,add_note,0) == "continue":
+	  print "skipping..."
+	  return
+	time.sleep(0.5)
+	print "++ clicked add note"
+
+	textarea = browser.find_element_by_id("custom-message")
+	textarea.send_keys(note_linkedin)
+	print "++ added the note"
+	time.sleep(1)
+
+	send = browser.find_element_by_css_selector('button.button-primary-large.ml3')
+	send.click()
+	#close_overlay(browser)
+	print "++ clicked send"
+	time.sleep(3)
+
+def send_invitations(browser,count):
+	wait_until_page_loads(browser,0)
+	clicked_buttons = 0
+	time.sleep(0.5)
+	buttons = browser.find_elements_by_css_selector("button.search-result__actions--primary.button-secondary-medium.m5")
+	number_buttons = len(buttons)
+	print "++ i found ",str(number_buttons)," buttons"
+	time.sleep(0.5)
+	for i in range(0,number_buttons):
+		if "Connect" == buttons[i].text:
+			clicking(browser,buttons[i])
+			clicked_buttons = clicked_buttons + 1
+
+	if number_buttons - clicked_buttons > 1:
+	  print "-- i didn't click all the buttons"
+	  if count > 2:
+	  	print "-- but i tried many times"
+		return clicked_buttons
+	  print "++ refreshing page"
+	  browser.refresh()
+	  wait_until_page_loads(browser,0)
+	  scroll_down(browser)
+	  time.sleep(0.5)
+	  send_invitations(browser,count+1)
+	return clicked_buttons
+
+def do_the_job(browser,page,last_crashed_page):
+	try:
+		print "[+] starting page: ",str(page)
+		#search
+		browser.get(SEARCH_URL.replace("keywords=","keywords="+args.searchTerm).replace("page=","page="+str(page)))
+		#scroll_down_to_the_bottom_of_the_page_so_that_all_the_results_fully_load
+		scroll_down(browser)
+		time.sleep(0.2)
+		#send_invitations_with_note
+		x = send_invitations(browser,0)
+		print "i clicked ",x," buttons in page ",str(page)
+		return x
+	except Exception as e:
+		print "-- this was raised:"
+		print e
+		if last_crashed_page == page:
+			print "-- skipping page ",page
+			return
+		else:
+			last_crashed_page = page
+			print "-- restarting last crashed page ", str(page)
+		do_the_job(browser,page,last_crashed_page)
+
+
+def main():
 	reload(sys)
 	sys.setdefaultencoding('utf8')
 
-	#parse_the arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument("email", help="linkedin email")
-	parser.add_argument("password", help="linkedin password")
-	parser.add_argument("searchTerm", help="The field of interest")
-	args = parser.parse_args()
-
 	#open_firefox_window
-	browser = webdriver.Firefox()
-
-	#open_linkedin_login_url
-	browser.get("https://linkedin.com/uas/login")
-
-	#type_the_login_and_pass_and_submit
-	login(browser,args)
-
-	#search_for_the_desired_field
-	search(browser,args.searchTerm)
-
-	#load_the_note_from_file
-	msg_linkedin = ""
-	f = open('msg_linkedin.txt','r')
-	msg_linkedin = f.read()
-	f.close()
-	msg_linkedin = unicode(msg_linkedin, errors='replace')
-	time.sleep(5)
-
-	#send_invitation_with_note
-	for i in range(1,6):
-		print "Starting page "+ str(i)
-		send_invitations(browser,msg_linkedin)
-		liste = get_list_people(browser)
-		if liste:
-			print "found "+str(len(liste))+" Tunisians"
-		print "[+] Success! Page " + str(i) + " Done"
-		next_page(browser)
-
+	browser = webdriver.Chrome()
+	time.sleep(1.5)
+	login(browser)
+	s = 0
+	for page in range(1,101):
+		s = s + do_the_job(browser,page,0)
+	print "-------------------------"
+	print "| Total clicked buttons: ",s
+	print "-------------------------"
 	#close_browser_window
-	browser.close()
+	browser.quit()
 
 if __name__ == '__main__':
-	Main()
+	main()
